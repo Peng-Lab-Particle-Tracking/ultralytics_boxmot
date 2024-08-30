98385d6f8059a3d2c435c7b64ca38dc4df00230e
Mike <yolov5.deepsort.pytorch@gmail.com>
2024-08-30 13:30:55 +0200

    Update predictor.py

--- ultralytics/engine/predictor.py
+++ ultralytics/engine/predictor.py
@@ -258,10 +258,7 @@ class BasePredictor:
 
                 # Postprocess
                 with profilers[2]:
-                    if isinstance(self.model, AutoBackend):
-                        self.results = self.postprocess(preds, im, im0s)
-                    else:
-                        self.results = self.model.postprocess(path, preds, im, im0s)
+                    self.results = self.postprocess(preds, im, im0s)
                 self.run_callbacks("on_predict_postprocess_end")
 
                 # Visualize, save, write results
