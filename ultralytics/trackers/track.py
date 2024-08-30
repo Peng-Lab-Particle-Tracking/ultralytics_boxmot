d055b138a582a5465ac0aa212f447e38fbdabb4d
Mike <yolov5.deepsort.pytorch@gmail.com>
2024-08-30 12:00:14 +0200

    delete reset

--- ultralytics/trackers/track.py
+++ ultralytics/trackers/track.py
@@ -71,7 +71,6 @@ def on_predict_postprocess_end(predictor: object, persist: bool = False) -> None
         tracker = predictor.trackers[i if is_stream else 0]
         vid_path = predictor.save_dir / Path(path[i]).name
         if not persist and predictor.vid_path[i if is_stream else 0] != vid_path:
-            tracker.reset()
             predictor.vid_path[i if is_stream else 0] = vid_path
 
         det = (predictor.results[i].obb if is_obb else predictor.results[i].boxes).cpu().numpy()
